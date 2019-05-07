d40f49d96928a57963f02a4d2d25ec66cb35574d
ConnorCairns <connor@connorcairns.xyz>
2019-05-07 23:59:56 +0100

    emails

--- sainsburys.py
+++ sainsburys.py
@@ -6,33 +6,32 @@ import os
 
 email_address = os.environ.get('email_address')
 email_password = os.environ.get('email_password')
-recipients = ['connor@connorcairns.xyz', 'gimblemish@gmail.com']
+recipients = ['connor@connorcairns.xyz']
 
 cheap = 2.5
-urls = ['https://www.sainsburys.co.uk/shop/gb/groceries/häagen-dazs-vanilla-500ml']
+flavours = []
+urls = ['https://www.sainsburys.co.uk/shop/gb/groceries/häagen-dazs-vanilla-500ml', 'https://www.sainsburys.co.uk/shop/gb/groceries/haagen-dazs-salted-caramel-500ml', "https://www.sainsburys.co.uk/shop/gb/groceries/häagen-dazs-strawberry-cheesecake-500ml", "https://www.sainsburys.co.uk/shop/gb/groceries/häagen-dazs-cookies---cream-500ml"]
 
 for url in urls:
     r  = requests.get(url.encode("utf-8").decode("latin-1"), allow_redirects=True)
     soup = BeautifulSoup(r.text, 'html.parser')
     price = soup.find("p", class_="pricePerUnit").get_text(strip="True")
 
-    if float(price[1:4]) < cheap:
-        msg = EmailMessage()
-        msg["Subject"] = "Haagen Bargins!"
-        msg["From"] = email_address
-        msg["To"] = 'connor@connorcairns.xyz'
-        msg.set_content("Haagen Dazs is cheap!")
+    if float(price[1:4]) > cheap:
+        name = url[59:].replace("-", " ").replace("500ml", "")
+        flavours.append(name)
 
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-            smtp.login(email_address, email_password)
-            smtp.send_message(msg)
-    else:
-        msg = EmailMessage()
-        msg["Subject"] = "Haagen Bargins!"
-        msg["From"] = email_address
-        msg["To"] = ", ".join(recipients)
-        msg.set_content("Haagen Dazs is not cheap!")
+if len(flavours) > 1:
+    msg = EmailMessage()
+    msg["Subject"] = "Haagen Bargins!"
+    msg["From"] = email_address
+    msg["To"] = ", ".join(recipients)
+    msg.set_content(f"""Haagen Dazs is cheap!
 
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-            smtp.login(email_address, email_password)
-            smtp.send_message(msg)
+    The following flavours are cheap:
+    {flavours}
+    """)
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(email_address, email_password)
+        smtp.send_message(msg)
