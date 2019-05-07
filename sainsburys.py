0ec62bb2cfdb23f16f5e34a0024f55465bb660cc
ConnorCairns <connor@connorcairns.xyz>
2019-05-07 23:42:47 +0100

    sends emails

--- sainsburys.py
+++ sainsburys.py
@@ -1,7 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
+import smtplib
+from email.message import EmailMessage
+import os
+
+email_address = os.environ.get('email_address')
+email_password = os.environ.get('email_password')
+recipients = ['connor@connorcairns.xyz', 'gimblemish@gmail.com']
+
 cheap = 2.5
-urls = ['https://www.sainsburys.co.uk/shop/gb/groceries/häagen-dazs-vanilla-500ml', 'https://www.sainsburys.co.uk/shop/gb/groceries/haagen-dazs-salted-caramel-500ml', 'https://www.sainsburys.co.uk/shop/gb/groceries/häagen-dazs-cookies---cream-500ml']
+urls = ['https://www.sainsburys.co.uk/shop/gb/groceries/häagen-dazs-vanilla-500ml']
 
 for url in urls:
     r  = requests.get(url.encode("utf-8").decode("latin-1"), allow_redirects=True)
@@ -9,6 +17,22 @@ for url in urls:
     price = soup.find("p", class_="pricePerUnit").get_text(strip="True")
 
     if float(price[1:4]) < cheap:
-        print("haha yes")
+        msg = EmailMessage()
+        msg["Subject"] = "Haagen Bargins!"
+        msg["From"] = email_address
+        msg["To"] = 'connor@connorcairns.xyz'
+        msg.set_content("Haagen Dazs is cheap!")
+
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+            smtp.login(email_address, email_password)
+            smtp.send_message(msg)
     else:
-        print("no")
+        msg = EmailMessage()
+        msg["Subject"] = "Haagen Bargins!"
+        msg["From"] = email_address
+        msg["To"] = ", ".join(recipients)
+        msg.set_content("Haagen Dazs is not cheap!")
+
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+            smtp.login(email_address, email_password)
+            smtp.send_message(msg)
